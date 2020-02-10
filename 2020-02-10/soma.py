dc55115f63f386b7b30e0f2151178a23daa00cee
Matheus Rocha <matheus_rocha98@outlook.com>
2020-02-10 08:28:49 -0300

    adicionando mais exercicios

--- 2020-02-10/soma.py
+++ 2020-02-10/soma.py
@@ -1,3 +1,10 @@
 '''
-Faça uma função
+Faça uma função recursiva que calcule a soma dos dígitos de um inteiro positivo n
+def sd(n): # sd(123) = 6 = 1 + 2 + 3
 '''
+
+def sd(n):
+    if n <= 9: return n #só um digito
+    return n % 10 + sd(n // 10)
+
+print(sd(123))
