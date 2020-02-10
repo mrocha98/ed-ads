e6a064e8e96bc9b1defe36decb77676ba1a07181
Matheus Rocha <matheus_rocha98@outlook.com>
2020-02-10 08:57:39 -0300

    tunando fibonacci

--- 2020-02-10/fibonacci.py
+++ 2020-02-10/fibonacci.py
@@ -3,12 +3,20 @@ calcule fibonacci de um inteiro n
 def fib(n): #fib(4) = 3
 '''
 
+'''
+f = {}
 def fib(n):
-    print(n)
+    if n <= 2: return 1
+    if n not in f:
+        f[n] = fib(n-1) + fib(n-2)
+    return f[n]
+'''
+
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def fib(n):
     if n <= 2: return 1
     return fib(n-1) + fib(n-2)
 
-#for k in range(1, 11):
-#    print(f'{k}: {fib(k)}')
 
-print(fib(10))
+print(fib(100))
