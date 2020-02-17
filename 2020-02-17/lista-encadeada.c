959cc7a277e8437e7dfc4d5d8653d914f0cd35d6
Matheus Rocha <matheus_rocha98@outlook.com>
2020-02-17 08:38:09 -0300

    CABEEEEÇA ombro joelho e pé

--- 2020-02-17/lista-encadeada.c
+++ 2020-02-17/lista-encadeada.c
@@ -38,6 +38,14 @@ struct cel
   Porque eu sempre insiro no início da lista encadeada, por motivos de performance, se inserisse no final, e alista fosse enorme,
   teria que andar todo mundo até chegar no fim.
   Inserindo no início, para ter 3, 5, 10, preciso inserir ao contrário, 10, 5, 3
+
+  Lembrem que eu tenho outras decisões de implementação: como testar lista vazia? como alterar o início da lista?
+
+  Vamos ver primeiro COM CABEÇA e depois comparar SEM CABEÇA para ver qual é melhor
+
+  CABEÇA É UMA ESTRUTURA DE DADOS QUE TORNA A FUNÇÃO DE INSERIR MUITO MELHOR
+  1) NÃO PRECISO TESTAR LISTA VAZIA, PORQUE PELO MENOS TEM SEMPRE A CABEÇA
+  2) NÃO PRECISO PASSAR PONTEIROS PARA PONTEIROS, JÁ QUE LST APONTA PARA A CABEÇA
 */
 
 int main(void)
--- ed-zip/Lista Ligada cria lista com cabeça.c
+++ ed-zip/Lista Ligada cria lista com cabeça.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct cel {
-       int conteudo;
-       struct cel *seg; /* seguinte */
+struct cel
+{
+    int conteudo;
+    struct cel *seg; /* seguinte */
 };
 
 typedef struct cel celula;
 
-void Imprima (celula *lst) {
-     celula *p;
-     for (p = lst->seg; p != NULL; p = p->seg)
-         printf ("%d\n", p->conteudo);
+void Imprima(celula *lst)
+{
+    celula *p;
+    for (p = lst->seg; p != NULL; p = p->seg)
+        printf("%d\n", p->conteudo);
 }
 
-void Insere(int y, celula *p){
+void Insere(int y, celula *p)
+{
     celula *nova;
     nova = malloc(sizeof(celula));
     nova->conteudo = y;
@@ -22,7 +25,8 @@ void Insere(int y, celula *p){
     p->seg = nova;
 }
 
-int main(void){
+int main(void)
+{
     int i;
     celula *lst;
     lst = malloc(sizeof(celula));
