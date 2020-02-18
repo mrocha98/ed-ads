d26f853f05fb0460408948418961ef000f416cde
Matheus Rocha <matheus_rocha98@outlook.com>
2020-02-18 08:38:48 -0300

    criando função de concatenação

--- 2020-02-18/lista-encad.c
+++ 2020-02-18/lista-encad.c
@@ -21,6 +21,7 @@ void imprime(Celula *p) {
     printf("\n\n");
 }
 
+// ex.4
 int menor(Celula *p) {
     int menor = p->prox->val;
     for (Celula *aux = p->prox; aux != NULL; aux = aux->prox) {
@@ -30,17 +31,45 @@ int menor(Celula *p) {
     return menor;
 }
 
+// ex.13
+void concatenaListas(Celula *p, Celula *q) {
+    Celula *aux = p->prox;
+    while(aux->prox != NULL)
+        aux = aux->prox;
+    aux->prox = q->prox;
+    q = p;
+}
+
+// ex.9
+void converteVetorParaLista() {
+
+}
+
 int main(void) {
-    Celula cabeca;
-    cabeca.prox = NULL;
-    Celula *lst = &cabeca;
-    aloca(10, lst);
-    aloca(11, lst);
-    aloca(12, lst);
-    aloca(13, lst);
-    aloca(14, lst);
-    aloca(15, lst);
-    printf("%d\n", menor(lst));
+    Celula cabeca1;
+    cabeca1.prox = NULL;
+    Celula *lst1 = &cabeca1;
+    aloca(10, lst1);
+    aloca(11, lst1);
+    aloca(12, lst1);
+    aloca(13, lst1);
+    aloca(14, lst1);
+    aloca(15, lst1);
+
+    Celula cabeca2;
+    cabeca2.prox = NULL;
+    Celula *lst2 = &cabeca2;
+    aloca(100, lst2);
+    aloca(200, lst2);
+    aloca(300, lst2);
+    aloca(400, lst2);
+    aloca(500, lst2);
+    aloca(600, lst2);
+
+    concatenaListas(lst1, lst2);
+    imprime(lst1);
+    imprime(lst2);
+
     system("pause");
     return 0;
 }
